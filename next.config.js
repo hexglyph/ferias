5eedd26c248fd7ec18de62939c0f108935c7c1d5
Daniel Niemietz Braz <dnbraz@prodam.sp.gov.br>
2024-12-21 19:23:35 -0300

    Rename next.config.ts to next.config.js, update configuration export style, and modify TypeScript include paths

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
     if (isServer) {
@@ -8,6 +6,6 @@ const nextConfig: NextConfig = {
     }
     return config
   },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
